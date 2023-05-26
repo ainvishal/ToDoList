121bc9872313de0636bf69eaa7f63a27129f89ea
ainvis <109056582+ainvishal@users.noreply.github.com>
2023-05-26 19:44:34 +0530

    edit functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,73 @@
-import React from 'react'
-import './index.css'
-import ToDo from './ToDo.js'
+import { useState } from "react";
+import { TodoData } from "./data";
 
+
+let nextId = 4;
+
+function ToDo(){
+  const [TodoList, setList] = useState(TodoData);
+  const [name, setname] = useState('');
+  const [Editstate, setedit] = useState(false);
+  const [EditId, setId] = useState(0);
+  const [Editname, setename] = useState('');
+
+  function handleClick(e){
+    e.preventDefault();
+    const newList = [
+      ...TodoList,
+      {
+        id:nextId,
+        text:name
+      }
+    ];
+    setList(newList);
+    setname('');
+    nextId++;
+  }
+
+  function handleDel(index){
+    const upList = TodoList.filter(item => index !== item.id);
+    setList(upList);
+    nextId--;
+  }
+
+  function handleEdit(index){
+    setedit(true);
+    setId(index);
+  }
+
+  function handleSave(){
+    setedit(false);
+    setId(0);
+    setename('');
+  }
+
+  return(
+    <>
+      <label htmlFor="item">
+        <input type='text' value={name} onChange={e => setname(e.target.value)} />
+        <button onClick={handleClick}>Add to List</button>
+      </label>
+      <div>
+        {TodoList.map(item => {
+          if(Editstate && item.id === EditId){
+            return(
+              <p key={item.id}>
+                <input type="text" value={Editname} onChange={e => {setename(e.target.value);}}/>
+                <button onClick={() => {item.text = Editname;handleSave()}}>Save</button>
+              </p>
+            );
+          }
+          else{
+          return(
+            <p key={item.id}>{item.text}<button onClick={() => handleEdit(item.id)}>Edit</button><button onClick={() => handleDel(item.id)}>Delete</button></p>
+          )
+          }
+        })}
+      </div>
+    </>
+  );
+}
 
 function List(){
   return(
@@ -21,5 +87,4 @@ export default function App(){
     <List/>
   );
 }
-
 
