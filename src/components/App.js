8f6fd8923129c6740905daaeca300f7df333984e
ainvis <109056582+ainvishal@users.noreply.github.com>
2023-05-28 18:12:35 +0530

    after tailwind css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TodoData } from "./data";
+import './index.css'
 
 
 let nextId = 4;
@@ -44,23 +45,23 @@ function ToDo(){
 
   return(
     <>
-      <label htmlFor="item">
-        <input type='text' value={name} onChange={e => setname(e.target.value)} />
-        <button onClick={handleClick}>Add to List</button>
+      <label htmlFor="item" className="m-2">
+        <input type='text' className=" border border-black mx-2 w-64" value={name} onChange={e => setname(e.target.value)} />
+        <button className="mx-2 bg-blue-400 text-violet-700 rounded-md w-24" onClick={handleClick}>Add to List</button>
       </label>
-      <div>
+      <div className="p-2 my-5">
         {TodoList.map(item => {
           if(Editstate && item.id === EditId){
             return(
               <p key={item.id}>
-                <input type="text" value={Editname} onChange={e => {setename(e.target.value);}}/>
+                <input type="text" className="border border-black" value={Editname} onChange={e => {setename(e.target.value);}}/>
                 <button onClick={() => {item.text = Editname;handleSave()}}>Save</button>
               </p>
             );
           }
           else{
           return(
-            <p key={item.id}>{item.text}<button onClick={() => handleEdit(item.id)}>Edit</button><button onClick={() => handleDel(item.id)}>Delete</button></p>
+            <p key={item.id} className="m-1 p-1 ">{item.text}<button onClick={() => handleEdit(item.id)} className="mx-1 bg-blue-400 text-violet-700 rounded-md">Edit</button><button onClick={() => handleDel(item.id)} className="mx-1 bg-blue-400 text-violet-700 rounded-md">Delete</button></p>
           )
           }
         })}
@@ -72,10 +73,10 @@ function ToDo(){
 function List(){
   return(
     <>
-    <div className="label">
+    <div className="text-center p-4 m-7 tracking-wide">
       <h2>To Do List</h2>
     </div>
-    <div className="content">
+    <div className="text-center p-2 m-5">
       <ToDo/>
     </div>
   </>
